b4ff70a3c2ae7a3a7e67a96538af43a9d7fb3b5b
eunkang-dyphi <eunkang@dyphi.com>
2024-11-15 08:35:05 +0900

    :sparkles: 	add: 객체를 리스폰스하는 API 예제 생성

--- springmvc/src/main/java/spring_study/springmvc/controllers/HelloController.java
+++ springmvc/src/main/java/spring_study/springmvc/controllers/HelloController.java
@@ -13,10 +13,37 @@ public class HelloController {
         model.addAttribute("data", "hello!");
         return "hello"; // resources/templates/hello.html 을 리턴해서 렌더링.
     }
+    @GetMapping("hello-mvc")
+    public String helloMVC(@RequestParam(value = "name") String name,  Model model) {
+        model.addAttribute("name", name);
+        return "hello-mvc";
+    }
 
     @GetMapping("hello-string")
     @ResponseBody
     public String helloString(@RequestParam("name") String name) {
+        // 단순문자열이면 StringConverter로 응답데이터를 만듦.
         return "hello" + name;
+    }
+
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        // HttpMessageConverter 로 객체를 json방식으로 응답데이터를 만듦
+        return hello; // 객체를 리턴 => 객체인경우에는 JSON으로 데이터를 만들어서 HTTP 응답으로 리턴
+    }
+    static class Hello {
+        private String name;
+
+        public String getName( ) {
+            return name;
+        }
+
+        public void setName( String name ) {
+            this.name = name;
+        }
     }
 }
