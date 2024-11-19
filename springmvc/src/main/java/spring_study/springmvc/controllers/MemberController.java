613e08fc30171088c1d3a3fa87846d7f713121dc
loveAlakazam <dmsrkd1216@gmail.com>
2024-11-19 18:39:09 +0900

    :memo: Update: 회원웹기능뷰 추가

--- springmvc/src/main/java/spring_study/springmvc/controllers/MemberController.java
+++ springmvc/src/main/java/spring_study/springmvc/controllers/MemberController.java
@@ -2,7 +2,13 @@ package spring_study.springmvc.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import spring_study.springmvc.domain.Member;
 import spring_study.springmvc.service.MemberService;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -13,6 +19,28 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    // 멤버생성 폼UI를 리턴
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+    // 신규멤버 생성후 홈화면 리턴
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+    // 회원목록 리턴
+    @GetMapping("/members")
+    public String memberList(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 
 }
