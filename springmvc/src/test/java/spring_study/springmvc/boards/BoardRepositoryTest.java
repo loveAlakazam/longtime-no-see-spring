e7ce0fae465dfbdd3233f5228f24f6c8a239a213
eunkang-dyphi <eunkang@dyphi.com>
2024-12-01 23:59:12 +0900

    :test_tube: Add: 테스트케이스 추가

--- springmvc/src/test/java/spring_study/springmvc/boards/BoardRepositoryTest.java
+++ springmvc/src/test/java/spring_study/springmvc/boards/BoardRepositoryTest.java
@@ -1,10 +1,11 @@
 package spring_study.springmvc.boards;
 
-import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import spring_study.springmvc.domain.Board;
 
@@ -13,18 +14,21 @@ import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@DataJpaTest
+@DataJpaTest // 테스트종료 데이터 초기화
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE) // 임베디드 데이터베이스 사용
 public class BoardRepositoryTest {
-
     // 공용 테스트 데이터
-    @Autowired
-    private BoardRepositoryImpl boardRepository;
+    private BoardRepository boardRepository;
 
+    @Autowired
+    public BoardRepositoryTest( BoardRepository boardRepository ) {
+        this.boardRepository = boardRepository;
+    }
 
 
     @Test
     @DisplayName( "게시글 생성" )
-    public void 게시글_생성_테스트() {
+    public void test_save() {
         // given
         Board board = new Board();
         board.setTitle( "테스트 게시글 제목" );
@@ -42,7 +46,7 @@ public class BoardRepositoryTest {
 
     @Test
     @DisplayName( "비어있을 경우 전체조회" )
-    public void 게시글_전체조회_테스트_1() {
+    public void test_findAll_1() {
         // given
         // when
         List<Board> boards = boardRepository.findAll();
@@ -52,7 +56,7 @@ public class BoardRepositoryTest {
     }
     @Test
     @DisplayName( "데이터가 들어있는 경우 전체조회" )
-    public void 게시글_전체조회_테스트_2() {
+    public void test_findAll_2() {
         // given
         for(int i=0; i< 10; i++) {
             Board board = new Board();
@@ -70,7 +74,7 @@ public class BoardRepositoryTest {
         assertThat( boards.size() ).isEqualTo( 10 );
     }
     @Test
-    public void 게시글_단건조회_테스트() {
+    public void test_findById() {
         // given
         Board board = new Board();
         board.setTitle( "테스트 게시글 제목" );
@@ -85,5 +89,18 @@ public class BoardRepositoryTest {
         // then
         assertThat( result.get() ).isNotNull();
     }
+
+
+    @BeforeEach
+    void beforeEach() {
+        // 테스트 이전 데이터베이스 데이터 삭제
+        boardRepository.deleteAll();
+    }
+    @AfterEach
+    void afterEach() {
+        // 테스트 이후 데이터베이스 데이터 삭제
+        boardRepository.deleteAll();
+    }
+
 
 }
--- springmvc/src/test/java/spring_study/springmvc/boards/BoardServiceTest.java
+++ springmvc/src/test/java/spring_study/springmvc/boards/BoardServiceTest.java
@@ -1,5 +1,6 @@
 package spring_study.springmvc.boards;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class BoardServiceTest {
@@ -9,19 +10,5 @@ public class BoardServiceTest {
         this.boardService = boardService;
     }
 
-    @Test
-    public void 게시글_생성_서비스로직_테스트() {
 
-
-    }
-
-    @Test
-    public void 게시글_전체조회_서비스로직_테스트() {
-
-    }
-
-    @Test
-    public void 게시글_상세조회_서비스로직_테스트() {
-
-    }
 }
